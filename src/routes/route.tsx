bbfaf57036c8ecf1e4333543eb3ee49fabc6d137
Michelle Setúbal <missetubal@outlook.com>
2022-09-19 10:50:32 -0300

    style and type helpers

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,14 +1,21 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {Home} from '../pages/Home/Home.screen';
+import {Intro} from '../pages/Intro/Intro.screen';
+import Register from '../pages/Register/Register.screen';
+import {Welcome} from '../pages/Welcome/Welcome.screen';
 
 export const Route: React.FC = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Intro" component={Intro} />
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
